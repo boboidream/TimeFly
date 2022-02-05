e6997fa7dc92cf9f57795d22088f581bc90451b7
boboidream <im.wbzhang@outlook.com>
2022-02-05 15:50:28 +0800

    [+] 0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
     docsDir: '/',
     lastUpdated: 'Last Updated',
   },
-  plugins: ['permalink-pinyin', ['autobar', {'pinyinNav': true, "stripNumbers": false, "maxLevel": 0}], 'rpurl'],
+  plugins: ['permalink-pinyin', ['autobar', {'pinyinNav': true}], 'rpurl'],
   chainWebpack: (config, isServer) => {
     const inlineLimit = 10000
     config.module.rule('images')
